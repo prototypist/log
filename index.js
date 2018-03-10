e51ead35176cd249e6a6bbcb5e52a091de70e7f6
bulx <sambulkley@gmail.com>
2018-03-09 18:23:46 -0800

    lint

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const levels = {
 };
 
 function log(level, message) {
-  console.log(`[${levels[level](level)}]`, message);
-};
+  console.log(`[${levels[level](level)}]`, message); // eslint-disable-line no-console
+}
 
 function system(message) {
   log('system', message);
-};
+}
 
 function info(message) {
   log('info', message);
-};
+}
 
 function success(message) {
   log('success', message);
-};
+}
 
 function warning(message) {
   log('warning', message);
-};
+}
 
 function error(message) {
   log('error', message);
-};
+}
 
 module.exports = {
   system, info, success, warning, error,
